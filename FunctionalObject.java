43da1b3f38fabc7a23a61bfc94613d6596a2d06d
sarveshped30 <sarveshpednekar30@gmail.com>
2022-05-08 17:57:07 +0530

    Added: Functional interface file

---
+++ FunctionalObject.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.demo;
+
+//FunctionalInterface with Args
+interface FunInterface {
+    void print3(int a, int b);
+}
+
+public class FunctionalObject {
+    public static void main(String[] args) {
+        // Object for FunInterface
+        FunInterface func = (int a, int b) -> {
+            System.out.println("*****FunctionalInterface with Args********");
+            int c = a+b;
+            System.out.println("Sum of " + a + " and " + b +" is: " + c );
+        };
+
+        func.print3(5, 6);
+    }
+}
+
+
+
+
